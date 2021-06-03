ddb611b417240d95cde5636c33251a95f968c36e
Jose Gavilán <josegavilanruiz@gmail.com>
2021-06-03 12:20:13 +0200

    Añadido mensaje de info para cuando el fichero externo aún no se ha generado

--- src/main/java/com/quimibot/handler/CovidHandler.java
+++ src/main/java/com/quimibot/handler/CovidHandler.java
@@ -14,17 +14,19 @@ public class CovidHandler implements Handler {
 
     @Override
     public void handle(long chatId, String messageTextReceived, SendMessage message) {
+    	String messagefromFile ="";
         try {
             SendMessage aviso = new SendMessage();
             aviso.setChatId(chatId).setText("Se están realizando los cálculos, espera...");
             longPollingBot.execute(aviso);
-            String messagefromFile = CovidService.getCovidInfoTop10FromFile();
+            messagefromFile = CovidService.getCovidInfoTop10FromFile();
             if(messagefromFile.equals("")){
             	System.out.println("No estoy leyendo del fichero");
             	messagefromFile = CovidService.getCovidInfoTop10();
             }
             message.setChatId(chatId).setText(messagefromFile);
         } catch (Exception ioe) {
+        	message.setChatId(chatId).setText("No se han podido obtener los datos, puede ser que aún no se hayan actualizado. Por favor, prueba de nuevo más tarde.");
             System.out.println("Se ha producido un error: ");
             ioe.printStackTrace();
         }
